36d00697cf5f15b8d6e0f64b4339421e49d8be6a
Nicolas Lima Freitas <nicolaslimafreitas7@gmail.com>
2025-05-11 20:59:11 -0300

    Criacao do projeto spring boot (BIBLITECA.JAVA) e classe livro

---
+++ src/main/java/com/biblioteca/biblioteca/model/Livro.java
@@ -0,0 +1,31 @@
+package com.biblioteca.biblioteca.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Livro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String autor;
+    private String categoria;
+    private boolean disponivel = true;
+
+    // Getters e setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getTitulo() { return titulo; }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+
+    public String getAutor() { return autor; }
+    public void setAutor(String autor) { this.autor = autor; }
+
+    public String getCategoria() { return categoria; }
+    public void setCategoria(String categoria) { this.categoria = categoria; }
+
+    public boolean isDisponivel() { return disponivel; }
+    public void setDisponivel(boolean disponivel) { this.disponivel = disponivel; }
+}
